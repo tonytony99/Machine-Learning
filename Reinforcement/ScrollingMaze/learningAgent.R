fde0b9fa567ef73ff022c1a4ecf739edc876baa8
Tony <anthony.bryan@outlook.com>
2017-12-22 16:13:59 +0000

    Make negative reward decay more quickly for stability

--- Reinforcement/ScrollingMaze/learningAgent.R
+++ Reinforcement/ScrollingMaze/learningAgent.R
@@ -1,8 +1,13 @@
+# This program was inspired by the popular Flappy Bird game
+# in which players try to get a bird through a series of scrolling
+# pipes. This program attempts to solve a problem similar to 
+# that of Flappy Bird using reinforcement learning.
+
 # Game set up
 
 # length of map
-reps = 1000
-# lowest and highest allowed bird height
+reps = 800
+# lowest and highest allowed agent height
 bottom = 0; top = 200
 # distance (time) between each pipe
 xDistBetweenPipes = 100
@@ -22,17 +27,17 @@ lowPipeTops = ifelse(pipeCentres>0, pipeCentres - yDistBetweenPipes/2,bottom)
 # pipeDist[t] : horizontal distance to next nearest pipe at time t
 pipeDist = rep(xDistBetweenPipes : 1, reps/xDistBetweenPipes)
 
-# heights[t] : height of bird at time t
-# taps[t] : whether or not tap at time t
+# heights[t] : height of agent at time t
+# ups[t] : whether or not to go up at time t
 # vs[t] : speed at time t
-vs = taps = heights = rep(0,reps)
+vs = ups = heights = rep(0,reps)
 
 
 isAlive = function(height, bottom, lowPipeTop, highPipeBottom, top) {
   return (height > bottom & height > lowPipeTop & height < highPipeBottom & height < top)
 }
 
-# split the bird height into groups of 55
+# split the agent height into groups of 55
 getHeightState = function(height) {
   return(floor((height)/(top-bottom+1)*55)+1)
 }
@@ -42,7 +47,7 @@ getPipeXDistState = function(pipeXDist) {
   return(floor((pipeXDist-1)/xDistBetweenPipes*55)+1)
 }
 
-# split the vertical distance between the bird and the next pipe into groups of 55
+# split the vertical distance between the agent and the next obstacle into groups of 55
 getPipeYDistState = function(pipeYDist) {
   minYDist = bottom - (top - minPipeCentreFromEdge)
   maxYDist = top - (bottom + minPipeCentreFromEdge)
@@ -53,22 +58,23 @@ states = data.frame("heightState" = rep(0,reps),"pipeXDistState"=rep(0,reps),"pi
 
 # Learning setup
 # height x pipeXDist x pipeYDist x tap
-# it's generally better to not tap then tap
-highReward = rep(2, 55*55*55); # for not tapping
-lowReward = rep(1, 55*55*55); # for tapping
+# it's generally better to not go up then go up
+highReward = rep(2, 55*55*55); # for not going up
+lowReward = rep(1, 55*55*55); # for going up
 rewards = array(c(highReward,lowReward),c(55,55,55,2))
 # how much reward do you get for each t you survive
 survivalReward = 1
 # punishment for loosing the game
-lossPunishment = -15000
+lossPunishment = -1000
 
-trainingIterations = 250
+trainingIterations = 2500
 experimentHeights = rep(heights,trainingIterations)
 experimentHeights = array(experimentHeights, c(length(heights),trainingIterations))
 experimentScores = rep(0,trainingIterations)
-punishmentDecay = 0.5
+# for each time step we go back, the punishment is multplied by this coefficient
+punishmentDecay = 0.3
 # if we loose, how many previous time steps are punished?
-iterationsToGoBack = 50
+iterationsToGoBack = 20
 longTermRewardDecay = 1
 longTermPunishmentDecay = 1
 
@@ -77,7 +83,7 @@ for (trainingIteration in 1:trainingIterations) {
   survivalReward = survivalReward * longTermRewardDecay
   lossPunishment = lossPunishment * longTermPunishmentDecay
   
-  vs = taps = heights = rep(0,reps)
+  vs = ups = heights = rep(0,reps)
   
   # START GAME
   v = 0     # velocity
@@ -86,7 +92,7 @@ for (trainingIteration in 1:trainingIterations) {
   t = 1     # time
   vs[t] = v
   heights[t] = height
-  taps[t] = 1
+  ups[t] = 1
   
   # UPDATE CURRENT STATE
   heightState = getHeightState(height)
@@ -107,13 +113,11 @@ for (trainingIteration in 1:trainingIterations) {
     lastHeightState = states[t-1,1]
     lastPipeXDistState = states[t-1,2]
     lastPipeYDistState = states[t-1,3]
-    oldReward = rewards[lastHeightState,lastPipeXDistState,lastPipeYDistState,taps[t-1]]
-    # rewards[lastHeightState,lastPipeXDistState,lastPipeYDistState,taps[t-1]] = oldReward + survivalReward
-    rewards[lastHeightState,lastPipeXDistState,lastPipeYDistState,taps[t-1]] = ((t-1)/(t))* oldReward + (1/t) * survivalReward
+    oldReward = rewards[lastHeightState,lastPipeXDistState,lastPipeYDistState,ups[t-1]]
+    # rewards[lastHeightState,lastPipeXDistState,lastPipeYDistState,ups[t-1]] = oldReward + survivalReward
+    rewards[lastHeightState,lastPipeXDistState,lastPipeYDistState,ups[t-1]] = ((t-1)/(t))* oldReward + (1/t) * survivalReward
     
-    
-    # print(paste("rewarding state : ",lastHeightState,lastPipeXDistState,lastPipeYDistState,taps[t-1]))
-    
+    # print(paste("rewarding state : ",lastHeightState,lastPipeXDistState,lastPipeYDistState,ups[t-1]))
     
     # UPDATE CURRENT STATE
     heightState = getHeightState(height)
@@ -127,20 +131,15 @@ for (trainingIteration in 1:trainingIterations) {
     states[t,] = c(heightState, pipeXDistState, pipeYDistState)
     
     # CHOOSE A MOVE
-    # if the best reward given the current state comes from tapping
+    # if the best reward given the current state comes from going up
     if (which.max(rewards[heightState, pipeXDistState,pipeYDistState,]) == 2) {
       # then tap
-      taps[t] = 2
+      ups[t] = 2
       v = 4
     }  else {
       # do not tap
-      taps[t] = 1
+      ups[t] = 1
     }
-    
-#    print(paste("t :", t, " rewards :",rewards[heightState, pipeXDistState,pipeYDistState,1],rewards[heightState, pipeYDistState,pipeYDistState,2],
-#                " states :",heightState, pipeXDistState,pipeYDistState," tap:",taps[t]))
-  
-    
     
     # UPDATE THE GAME
     v = v + a
@@ -150,18 +149,17 @@ for (trainingIteration in 1:trainingIterations) {
     
   }
   
-  
   currentPunishment = lossPunishment
   for (i in 0:iterationsToGoBack) {
     # PUNISH THE MOVES THAT LEAD UP TO THE LOSS
     lastHeightState = states[t-i,1]
     lastPipeXDistState = states[t-i,2]
     lastPipeYDistState = states[t-i,3]
-    oldReward = rewards[lastHeightState,lastPipeXDistState,lastPipeYDistState,taps[t-i]]
-    # rewards[lastHeightState,lastPipeXDistState,lastPipeYDistState,taps[t-i]] = oldReward + currentPunishment
-    rewards[lastHeightState,lastPipeXDistState,lastPipeYDistState,taps[t-i]] = ((t-1)/(t)) * oldReward + (1/t) * currentPunishment
+    oldReward = rewards[lastHeightState,lastPipeXDistState,lastPipeYDistState,ups[t-i]]
+    # rewards[lastHeightState,lastPipeXDistState,lastPipeYDistState,ups[t-i]] = oldReward + currentPunishment
+    rewards[lastHeightState,lastPipeXDistState,lastPipeYDistState,ups[t-i]] = ((t-1)/(t)) * oldReward + (1/t) * currentPunishment
     currentPunishment = currentPunishment * punishmentDecay
-    # print(paste("punishing state : ",lastHeightState,lastPipeXDistState,lastPipeYDistState,taps[t]," for ",currentPunishment))
+    # print(paste("punishing state : ",lastHeightState,lastPipeXDistState,lastPipeYDistState,ups[t]," for ",currentPunishment))
     
   }
   
@@ -180,15 +178,12 @@ for (j in seq(mA,1,by=-50)) {
 }
 
 # Plot all of the performances against each other
-plot(experimentHeights[,mA],xlim=c(0,experimentScores[mA]), type = "l", ylim=c(bottom,top),ylab="height",main="All Bird Paths")
+plot(experimentHeights[,mA],xlim=c(0,experimentScores[mA]), type = "l", ylim=c(bottom,top),ylab="height",main="All Agent Paths")
 lines(highPipeBottoms, col = "green")
 lines(lowPipeTops, col = "green")
 for (j in mA:1) {
   lines(experimentHeights[,j],col=rgb(1.0,0.0,0.0,0.5))
 }
 
-
 # how did the program do with each iteration?
 plot(experimentScores,type="l",xlab="Iteration",ylab="Score",main="Score Improvement Over Time")
-
-
